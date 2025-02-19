1f79dee01cc529baec83ff720ce27b1cccb00ff9
Sabneet Bains <45975033+sabneet95@users.noreply.github.com>
2025-02-19 18:21:32 -0500

    Update Exponential_Map.m

--- Exponential_Map.m
+++ Exponential_Map.m
@@ -1,4 +1,5 @@
-% Exponential_Map.m - Bifurcation diagram simulation
+function exponential_map_main()
+% exponential_map_main - Bifurcation diagram simulation for the exponential map
 %
 % This script simulates the map:
 %   x(n+1) = x(n) * exp(lambda*(1 - x(n)))
@@ -9,48 +10,60 @@
 % Author: Sabneet Bains
 % License: MIT License
 
-clear; close all; clc;
-
-% Initialize arrays to store simulation results
-xResults = [];
-lambdaResults = [];
-
-% Define parameter range for lambda
-lambdaVals = 1.5:0.001:4;
-
-% Simulation parameters
-numIterations = 512;     % Total iterations per lambda
-transientCutoff = 255;   % Remove the first 255 iterations (i.e., keep from 256th onward)
-
-% Loop over each lambda value
-for lambda = lambdaVals
-    % Pre-allocate vector for iterates
-    xn = zeros(numIterations, 1);
-    
-    % Set initial condition and compute the first iterate
-    x0 = 0.5;
-    xn(1) = x0 * exp(lambda*(1 - x0));
-    
-    % Iterate the map using a for-loop
-    for i = 1:numIterations-1
-        xn(i+1) = xn(i) * exp(lambda*(1 - xn(i)));
+    try
+        %% Initialization
+        clear; close all; clc;
+        
+        % Preallocate arrays to store simulation results
+        xResults = [];
+        lambdaResults = [];
+        
+        % Define parameter range for lambda
+        lambdaVals = 1.5:0.001:4;
+        
+        % Simulation parameters
+        numIterations = 512;      % Total iterations per lambda
+        transientCutoff = 255;    % Remove the first 255 iterations
+        
+        %% Main Loop: Iterate the map for each lambda
+        for lambda = lambdaVals
+            % Preallocate the vector for iterates
+            xn = zeros(numIterations, 1);
+            
+            % Set the initial condition and compute the first iterate
+            x0 = 0.5;
+            xn(1) = x0 * exp(lambda * (1 - x0));
+            
+            % Iterate the map using a for-loop
+            for i = 1:(numIterations - 1)
+                xn(i+1) = xn(i) * exp(lambda * (1 - xn(i)));
+            end
+            
+            % Remove transient dynamics
+            xn_trimmed = xn(transientCutoff+1:end);
+            
+            % Create a vector of lambda values corresponding to xn_trimmed
+            lambda_vector = lambda * ones(size(xn_trimmed));
+            
+            % Append the results for this lambda
+            xResults = [xResults; xn_trimmed];
+            lambdaResults = [lambdaResults; lambda_vector];
+        end
+        
+        %% Plotting the Bifurcation Diagram
+        figure;
+        plot(lambdaResults, xResults, '.', 'MarkerSize', 1);
+        xlabel('\lambda');
+        ylabel('x_n');
+        title('Bifurcation Diagram for x(n+1) = x(n) * exp(\lambda (1 - x(n)))');
+        grid on;
+        
+    catch ME
+        fprintf('An error occurred: %s\n', ME.message);
     end
-    
-    % Remove transient dynamics
-    xn_trimmed = xn(transientCutoff+1:end);
-    
-    % Create a vector of lambda values (same size as xn_trimmed)
-    lambda_vector = lambda * ones(size(xn_trimmed));
-    
-    % Append results for this lambda
-    xResults = [xResults; xn_trimmed];
-    lambdaResults = [lambdaResults; lambda_vector];
 end
 
-% Plot the bifurcation diagram: lambda vs. x(n)
-figure;
-plot(lambdaResults, xResults, '.', 'MarkerSize', 1);
-xlabel('\lambda');
-ylabel('x_n');
-title('Bifurcation Diagram for x(n+1) = x(n) * exp(\lambda (1 - x(n)))');
-grid on;
+% Run the main function if this script is executed directly
+if ~isdeployed
+    exponential_map_main();
+end
